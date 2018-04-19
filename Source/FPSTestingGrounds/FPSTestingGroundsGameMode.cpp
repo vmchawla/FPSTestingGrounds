805e634a8c91fbf1dbddf6952036187ca7fdec80
vmchawla <vmchawla158kb@hotmail.com>
2018-04-19 15:08:40 +0530

    235 Refactor Complete Clean project

--- Source/FPSTestingGrounds/FPSTestingGroundsGameMode.cpp
+++ Source/FPSTestingGrounds/FPSTestingGroundsGameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "FPSTestingGroundsGameMode.h"
 #include "FPSTestingGroundsHUD.h"
-#include "FPSTestingGroundsCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AFPSTestingGroundsGameMode::AFPSTestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Blueprints/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
---
+++ Source/FPSTestingGrounds/NPC/ChooseNextWaypoint.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "PatrolRouteComponent.h"
+
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
+{
+
+	//TODO protect against no Patrol Route component
+
+	//TODO protect agaisnt empty patrol routes
+
+
+
+	//Initialise local variable
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRouteComponent>();
+
+	if (!PatrolRoute)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No patrol route component found. Forget to attack patrol route?"));
+		return EBTNodeResult::Failed;
+	}
+
+	PatrolPoints = PatrolRoute->GetPatrolPoints();
+
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No routes set on Patrol route component"));
+		return EBTNodeResult::Failed;
+	}
+
+	//set current waypoint and next waypoint
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(CurrentWaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+
+	//cycle index
+	int32 modulo = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, modulo);
+	BlackboardComp->SetValueAsObject(NextWaypointKey.SelectedKeyName, PatrolPoints[modulo]);
+
+	return EBTNodeResult::Succeeded;
+}
+
+ 
